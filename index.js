b05e15301b461f0a9fdda4b929b8f82635f66247
dmiric <damir.miric@gmail.com>
2021-05-24 15:21:38 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function sendSelection(selection) {
+    console.log(selection)
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "webhook", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("Accept", "application/json");
+    xhttp.send(JSON.stringify(selection));
+}
+
+miro.onReady(() => {
+    miro.addListener('SELECTION_UPDATED', (e) => {
+        sendSelection(e.data)
+    })
+    miro.board.selection.get()
+      .then(sendSelection)
+})
