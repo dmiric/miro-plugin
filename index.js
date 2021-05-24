799226cda8f838f0714f69d5af4ed138c30f5a7d
dmiric <damir.miric@gmail.com>
2021-05-24 18:44:38 +0200

    Add port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function sendSelection(selection) {
     console.log(selection)
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://localhost/miro-webhook", true);
+    xhttp.open("POST", "http://localhost:3213/miro-webhook", true);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.setRequestHeader("Accept", "application/json");
     xhttp.send(JSON.stringify(selection));
