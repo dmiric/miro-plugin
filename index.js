c0536d9dce0cc602318ee87d4ea71b2752626be3
dmiric <damir.miric@gmail.com>
2021-05-25 09:19:21 +0200

    Remove unnecessary Accept header

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function sendSelection(selection) {
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "http://localhost:3213/miro-webhook", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.setRequestHeader("Accept", "application/json");
     xhttp.send(JSON.stringify(selection));
 }
 
